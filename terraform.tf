882e92e0f4a0a35fd6f273fa1a00f42952a27884
Onkar2515 <163623606+Onkar2515@users.noreply.github.com>
2024-06-27 10:32:05 +0530

    Update and rename terraform.io to terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,7 @@
 provider "aws" {
-  region="us-west-2"
-  access_key
+  region= "us-west-2"
+  access_key = "AKIA47CRWAQBYT7GWPNB"
+  secret_key = "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"
 }
 resource "aws_instance" "server01" {
   ami       = "ami-0cf2b4e024cdb6960"
