e4d864740a6ba6429e09b5f36a10df26016e8679
Onkar2515 <163623606+Onkar2515@users.noreply.github.com>
2024-06-28 17:08:53 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -7,11 +7,12 @@ resource "aws_instance" "server4" {
   ami       = "ami-08012c0a9ee8e21c4"
   instance_type = "t2.micro"
 
-  user_data =
+  user_data = <<-EOF
     #!/bin/bash
     yum update -y
     yum install -y httpd
     systemctl start httpd
     systemctl enable httpd
-    echo "<h1>Hello Guys..!!</h1>" /var/www/html/index.html
+    echo "<h1>Hello Guys..!!</h1>" > /var/www/html/index.html
+    EOF
 }
