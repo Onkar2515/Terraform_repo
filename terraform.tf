06660e6fc27b5a0d8289d2852227de05834c59ec
Onkar2515 <163623606+Onkar2515@users.noreply.github.com>
2024-06-28 17:01:27 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -6,4 +6,12 @@ provider "aws" {
 resource "aws_instance" "server4" {
   ami       = "ami-08012c0a9ee8e21c4"
   instance_type = "t2.micro"
+
+  user_data =
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+    echo "<h1>Hello Guys..!!</h1>" /var/www/html/index.html
 }
