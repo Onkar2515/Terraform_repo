c0c533e7c85d74d9aaa1c0d81f8250b8b85eb5be
Onkar2515 <163623606+Onkar2515@users.noreply.github.com>
2024-07-07 19:06:44 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,16 +3,14 @@ provider "aws" {
   AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
   region= "us-west-1"
 }
-resource "aws_instance" "server4" {
-  ami       = "ami-08012c0a9ee8e21c4"
+resource "aws_instance" "server11" {
+  ami       = "ami-0ff591da048329e00"
   instance_type = "t2.micro"
 
-  user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y httpd
-    systemctl start httpd
-    systemctl enable httpd
-    echo "<h1>Hello Guys..!!</h1>" > /var/www/html/index.html
-    EOF
+  user_data = <<EOF
+#!/bin/bash
+sudo apt update -y
+sudo apt install -y apache2
+sudo echo "Hello Guys..!!! How are you" > /var/www/html/index.html
+EOF
 }
