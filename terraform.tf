cda12668711c19d66b09a99ca6e7a1607d5332ec
Onkar2515 <163623606+Onkar2515@users.noreply.github.com>
2024-07-07 19:22:25 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -6,11 +6,4 @@ provider "aws" {
 resource "aws_instance" "server11" {
   ami       = "ami-0ff591da048329e00"
   instance_type = "t2.micro"
-
-  user_data = <<EOF
-#!/bin/bash
-sudo apt update -y
-sudo apt install -y apache2
-sudo echo "Hello Guys..!!! How are you" > /var/www/html/index.html
-EOF
 }
