089bd355a222746e2d7fabf1e76f53ef4d5c6bc2
Onkar2515 <163623606+Onkar2515@users.noreply.github.com>
2024-07-08 12:34:45 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ provider "aws" {
   secret_key = "VKeuWLKCiXSBWCMlEdl6HHKsNdrynsxh8szCifnw"
   region= "us-west-1"
 }
-resource "aws_instance" "server12" {
+resource "aws_instance" "server13" {
   ami       = "ami-0ff591da048329e00"
   instance_type = "t2.micro"
 }
