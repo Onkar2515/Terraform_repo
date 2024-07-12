89738366d949bce3ec208cd60043f0a3bbd061b3
Onkar2515 <163623606+Onkar2515@users.noreply.github.com>
2024-07-12 12:58:36 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,13 @@ provider "aws" {
   secret_key = ""
   region= "us-west-1"
 }
-resource "aws_instance" "server13" {
+resource "aws_instance" "server3" {
   ami       = "ami-0ff591da048329e00"
   instance_type = "t2.micro"
+  user_data = <<EOF
+#!/bin/bash
+sudo apt update -y
+sudo apt install -y apache2
+sudo echo "Hello Guys" > /var/www/html/index.html
+EOF
 }
